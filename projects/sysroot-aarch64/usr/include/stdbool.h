e110bb9ea241944e3c53bead591e6fbc97919fda
w-run <wang@run.al>
2026-07-29 12:59:09 +0800

    cleanup: remove tracked projects/sysroot-* (duplicate multiarch sysroots)

--- projects/sysroot-aarch64/usr/include/stdbool.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEUOS_STDBOOL_H
-#define MEUOS_STDBOOL_H
-
-#define bool _Bool
-#define true 1
-#define false 0
-#define __bool_true_false_are_defined 1
-
-#endif
--- projects/sysroot-riscv64/usr/include/stdnoreturn.h
+++
@@ -1,6 +0,0 @@
-#ifndef MEUOS_STDNORETURN_H
-#define MEUOS_STDNORETURN_H
-
-#define noreturn _Noreturn
-
-#endif
